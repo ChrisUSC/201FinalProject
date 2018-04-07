f6e7436792cdf468ab9b2ab6cea57600feae617f
Eric Yihan Chen <ericyihc@usc.edu>
2018-04-07 16:08:44 -0700

    Update Leaderboard.java
    Add networking function

--- Leaderboard/src/Leaderboard.java
+++ Leaderboard/src/Leaderboard.java
@@ -1,19 +1,26 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.ArrayList;
 import controlP5.*;
 import processing.core.PApplet;
 import processing.core.PFont;
+import java.io.*;
 
-public class Leaderboard extends PApplet{
+public class LeaderBoard extends PApplet{
 	
 	ArrayList<Integer> rank;
 	ArrayList<String> name;
 	ArrayList<Integer> score;
 	ControlP5 cp5;
+	ServerSocket ss = null;
 	boolean t = false;
+	private BufferedReader br;
+	boolean a = false;
+	private static ObjectInputStream ois;
 	
 	public void settings() {
 		  size(800, 800);
-		}
+	}
 	
 	public void setup() {
 		PFont font = createFont("arial",25);
@@ -30,7 +37,7 @@ public class Leaderboard extends PApplet{
 			cp5.addListBox(Integer.toString(500-(50*i))).setPosition(500,200+(50*i)).setSize(0,0).setFont(font);
 		}
 		
-		cp5.addListBox("A").setPosition(300,250).setSize(0,0).setFont(font);
+		cp5.addListBox("A").setPosition(300,250).setSize(100,100).setFont(font);
 		cp5.addListBox("B").setPosition(300,300).setSize(0,0).setFont(font);
 		cp5.addListBox("C").setPosition(300,350).setSize(0,0).setFont(font);
 		cp5.addListBox("D").setPosition(300,400).setSize(0,0).setFont(font);
@@ -40,15 +47,34 @@ public class Leaderboard extends PApplet{
 	
 	public void draw() {
 		background(0);
+		// TODO: add display stuff here, and read from the data structure
 	}
 	
-	
 	public static void main(String[] args) {
-		PApplet.main("Leaderboard");
+		//PApplet.main("LeaderBoard");
+		try {
+			System.out.println("Binding to port 6789");
+			ServerSocket ss = new ServerSocket(6789);
+			System.out.println("Bound to port " + 6789);
+			while(true) {
+				Socket s = ss.accept(); // blocking
+				System.out.println("Connection from: " + s.getInetAddress());
+				ois = new ObjectInputStream(s.getInputStream());
+				ScoreMsg sm = (ScoreMsg)ois.readObject();
+				System.out.println(sm.getScore());
+				System.out.println(sm.getUsername());
+				// TODO: add sm to a datastructure
+				PApplet.main("LeaderBoard");
+			}
+		} catch (IOException ioe) {
+			System.out.println("ioe in ChatRoom constructor: " + ioe.getMessage());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void populate(ArrayList<User> users) {
-		
 		ArrayList<Integer> rank;
 		ArrayList<String> name;
 		ArrayList<Integer> score;
