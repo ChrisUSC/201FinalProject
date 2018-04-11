15eaab2a51998b3e64ba15b2176c4fb14ab23b18
Damiano Carrioli <carrioli@usc.edu>
2018-04-10 17:23:45 -0700

    Add files via upload

---
+++ BlackHole.java
@@ -0,0 +1,17 @@
+package main;
+
+import processing.core.PApplet;
+
+public class BlackHole {
+	private PApplet parent;
+
+	BlackHole(PApplet parent) {
+		this.parent = parent;
+	}
+
+	void display(float x, float y) {
+		parent.fill(0);
+		parent.ellipse(x, y, 60, 60);
+	}
+
+}
