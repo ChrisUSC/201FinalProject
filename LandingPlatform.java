d350a9b34213d38c2d725bfb80f2af772bffb231
Damiano Carrioli <carrioli@usc.edu>
2018-04-15 16:28:16 -0700

    Update LandingPlatform.java

--- LandingPlatform.java
+++ LandingPlatform.java
@@ -8,7 +8,6 @@ public class LandingPlatform {
 	private float width;
 	private float height;
 	private PApplet parent;
-	private int level;
 	
 	public LandingPlatform(PApplet p, int xpos, int ypos, int width, int height){
 		parent = p;
@@ -16,24 +15,11 @@ public class LandingPlatform {
 		this.ypos = ypos;
 		this.width = width;
 		this.height = height;
-		this.level = 1;
-		
 	}
 	public void display() {
 		parent.fill(200, 200, 200);
 		parent.rect(xpos, ypos, width, height);
 	}
 	
-	public void changeWidth() {
-		this.width = width - 7;
-	}
-	
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
 
 }
